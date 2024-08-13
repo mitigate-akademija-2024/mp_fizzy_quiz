874f7ae21a88ecdaf39f8248da487726b01d7e9c
MuchMarts <98103825+MuchMarts@users.noreply.github.com>
2024-08-13 10:33:26 +0000

    Updated navigation and show quiz views

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -7,4 +7,21 @@ class Quiz < ApplicationRecord
   has_many :quiz_comments, dependent: :destroy
   has_many :user_scores, dependent: :destroy
   has_one :quiz_shared, dependent: :destroy
+
+  def description_preview
+    self.description.length > 30 ? "#{ self.description[0...27] }..." : self.description
+  end
+
+  def completed
+    user_scores.count
+  end
+
+  def max_correct_answers
+    user_scores.order(correct_count: :desc).first.correct_count
+  end
+
+  def max_score
+    user_scores.order(total_score: :desc).first.total_score
+  end
+
 end
