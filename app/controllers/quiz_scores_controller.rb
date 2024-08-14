44fa1563470e5ef859aa44fdd090df85f7d304d8
MuchMarts <98103825+MuchMarts@users.noreply.github.com>
2024-08-14 13:00:36 +0000

    Show answers, And breakdown

--- app/controllers/quiz_scores_controller.rb
+++ app/controllers/quiz_scores_controller.rb
@@ -1,14 +1,37 @@
 class QuizScoresController < ApplicationController
     before_action :authenticate_user!
     before_action :set_quiz
-    before_action :finished_quiz?, only: [:submit, :start]
-    
+    before_action :finished_quiz?, only: [:start, :submit]
     def start
         @questions = @quiz.questions
     end
 
     def show
-        @user_score = @quiz.user_scores.where(:user_id => current_user.id).first
+        @user_score = @quiz.user_scores.where(user: current_user).last
+        @questions = @quiz.questions
+        @total_correct = 0
+        
+        @user_answers = []
+
+        @questions.each do |question|
+            clean_user_answers = []
+            
+            question.answers.each do |a|
+                
+                if current_user.user_answers.where(user_score: @user_score, answer: a).first
+                    clean_user_answers.append(a)
+                end
+            end
+
+            @user_answers.append(
+                question: question,
+                user_answers: clean_user_answers 
+            )
+        end
+
+        @questions.each do |question|
+            @total_correct += question.answers.count(:correct)
+        end
     end
 
     def submit
@@ -16,18 +39,28 @@ class QuizScoresController < ApplicationController
         @user_score = UserScore.new(user: current_user, quiz: @quiz)
 
         answer_params.each do |question_id, answer_data|
-            answer_data.each do |id, correct|
-                @user_score.user_answers.build(
-                    user: current_user,
-                    answer_id: id,
-                )
+            answer_data.each do |id, chosen|
+                if chosen == "1"
+                    @user_score.user_answers.build(
+                        user: current_user,
+                        answer_id: id,
+                    )
+                end
+            end
+        end
+        
+        answer_count = 0
+
+        @user_score.user_answers.each do |answer|
+            if answer.answer.correct
+                answer_count += 1
             end
         end
 
-        @user_score.correct_count = @user_score.user_answers.where(correct: true).count   
-
+        @user_score.correct_count = answer_count
+        
         if @user_score.save
-            redirect_to result, notice: "Quiz submitted successfully."
+            redirect_to quiz_show_path, notice: "Quiz submitted successfully."
         else
             render :start, alert: "There was an error submitting your quiz!"
         end
